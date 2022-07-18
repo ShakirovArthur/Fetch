5980a084d938c5a81bc73be6d9b782f47aea107b
Artur Shakirov <88730967+ketrish@users.noreply.github.com>
2022-07-18 21:00:36 +0300

    1

--- src/App.js
+++ src/App.js
@@ -2,12 +2,34 @@ import React, { Component } from "react";
 import "./styles.css";
 
 export default class Pokemon extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+  }
   componentDidMount() {
     fetch("https://pokeapi.co/api/v2/pokemon/?limit=25")
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((result) => {
+        this.setState({
+          items: result.results
+        });
+      });
   }
+
   render() {
-    return <div className="App"></div>;
+    const { items } = this.state;
+    return (
+      <div className="App">
+        <ul>
+          {items.map((item) => (
+            <li>
+              <a href={item.url}>{item.name}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
